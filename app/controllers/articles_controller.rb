adea06e2d8bbccb8d2a5ee2445094c647edb6a3b
Thofeeq <thofeeqm@gmail.com>
2018-11-09 19:57:02 -0700

    Added edit action to ArticleController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
   private
     def article_params
       params.require(:article).permit(:title, :body)
