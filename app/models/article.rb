add2a6cf37af8ab000b27ee328e8edfa3c45f61b
Thofeeq <thofeeqm@gmail.com>
2018-11-09 18:13:20 -0700

    Added show/create actions for ArticleController

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum:1, maximum:50 }
-  validates :body, presence: true, length: {minimum:1, maximum:800}
+  validates :body, presence: true, length: { minimum:1, maximum:800 }
 end
