52ad1d72d3bf1b5327abfbe733c438a1014cc37d
Thofeeq <thofeeqm@gmail.com>
2018-11-11 21:39:50 -0700

    Refactoring - moved repeating codes in controller into method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :get_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -17,21 +19,17 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path 
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to article_path(@article)
     else
@@ -39,6 +37,9 @@ class ArticlesController < ApplicationController
     end
   end
   private
+    def get_article
+    @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :body)
     end
